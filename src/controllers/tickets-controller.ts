b1ce884982f34c12bfdecf8ce34474b437d68985
rodrigocqb <rodrigocortibarros@gmail.com>
2022-11-19 15:16:13 -0300

    refactor: Refactored postCreateTicket function to fit app standard

--- src/controllers/tickets-controller.ts
+++ src/controllers/tickets-controller.ts
@@ -29,16 +29,15 @@ export async function postCreateTicket(req: AuthenticatedRequest, res: Response)
   const { userId } = req;
   const { ticketTypeId } = req.body;
 
-  if (!ticketTypeId) {
-    return res.sendStatus(httpStatus.BAD_REQUEST);
-  }
-
   try {
     const enrollment = await enrollmentsService.getOneWithAddressByUserId(userId);
     const ticket = await ticketsService.createTicketWithEnrollmentAndTicketType(enrollment.id, ticketTypeId);
 
     return res.status(httpStatus.CREATED).send(ticket);
   } catch (error) {
-    return res.sendStatus(httpStatus.NOT_FOUND);
+    if (error.name === "NotFoundError") {
+      return res.sendStatus(httpStatus.NOT_FOUND);
+    }
+    return res.sendStatus(httpStatus.BAD_REQUEST);
   }
 }
